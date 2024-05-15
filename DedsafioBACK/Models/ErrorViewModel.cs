4fb62fe16cac80f657c12428224fcd38396cfd40
KarinaVillaFraternitas <karina.villa@fraternitas.io>
2024-05-15 15:53:25 -0600

    first commit

---
+++ DedsafioBACK/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DedsafioBACK.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
